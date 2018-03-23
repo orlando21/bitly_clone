dff0d3a82d58a7c497be7e72233e38e267f8de0b
orlando21 <me@gregbabb.com>
2018-03-23 17:43:19 +0100

    Add index route functionality

--- db/migrate/20180323152845_create_urls.rb
+++ db/migrate/20180323152845_create_urls.rb
@@ -1,6 +1,9 @@
 class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
+      t.integer :click_count
+      t.text :long_url
+      t.string :short_url
 
       t.timestamps
     end
