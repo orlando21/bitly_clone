b4338939dab075baa51bdb9b913ccfe4b592f468
orlando21 <me@gregbabb.com>
2018-03-23 18:36:42 +0100

    Add new link and delete functions

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,7 +1,24 @@
 class UrlsController < ApplicationController
 
+  include UrlsHelper
+
   def index
     @urls = Url.all
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
+    @url.save
+    redirect_to urls_path(@urls)
+  end
+
+  def destroy
+    @url = Url.destroy(params[:id])
+    redirect_to urls_path(@urls)
   end
 
 end
--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,2 +1,7 @@
 module UrlsHelper
+
+  def url_params
+    params.require(:url).permit(:long_url, :short_url)
+  end
+
 end
