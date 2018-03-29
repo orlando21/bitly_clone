9121350463c5ac6c58301222a3333ff1883b3d25
orlando21 <me@gregbabb.com>
2018-03-29 16:29:13 +0200

    Fix urls belonging to specific user

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   root to: 'urls#index'
   resources :urls
-  get '/:short_url' => 'urls#short_url_route', :constraints => {:short_url => /(?!.*?login).*/}
+  get '/:short_url' => 'urls#short_url_route', :constraints => {:short_url => /(?!.*?login|register).*/}
   resources :users
   resources :user_sessions, only: [ :new, :create, :destroy ]
 
   get 'login'  => 'user_sessions#new'
   delete 'logout' => 'user_sessions#destroy'
-  get 'register' => 'users_controller#new'
+  get 'register' => 'users#new'
 
 end
